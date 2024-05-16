a8084719dd99f54e316a4a2b97d7485168cb0d6b
Apsz <oddonodeonas@gmail.com>
2024-05-16 16:18:51 +0300

    wow that was really cool

--- client/preview/CThemes/branding/menu.js
+++ client/preview/CThemes/branding/menu.js
@@ -1,24 +1,60 @@
 $(document).ready(function() {
+
+  // Click handler for singleplayer button
   $('.singleplayer').click(function() {
-      $('#singleplayer').toggleClass('off on');
-      $('#multiplayer').addClass('off').removeClass('on');
-      $('#mainmenu').addClass('skibidi').removeClass('not-skibidi');
-      // Check if every element is off
-      if ($('#singleplayer').hasClass('off') && $('#multiplayer').hasClass('off')) {
-          $('#mainmenu').removeClass('skibidi').addClass('not-skibidi');
-      }
+    $('#singleplayer').toggleClass('off on'); // Toggle classes on clicked element (singleplayer)
+    $('#multiplayer').addClass('off').removeClass('on'); // Set multiplayer to off
+    $('#settings').addClass('off').removeClass('on');  // Set settings to off (new)
+    $('#accounts').addClass('off').removeClass('on');  // Set accounts to off (new)
+    $('#mainmenu').addClass('skibidi').removeClass('not-skibidi'); // Show main menu
+    
+    // Check if all menu buttons are off (singleplayer, multiplayer, settings, accounts)
+    if ($('#singleplayer').hasClass('off')) {  // Use class for all buttons
+      $('#mainmenu').removeClass('skibidi').addClass('not-skibidi'); // Hide main menu
+    }
   });
 
+  // Click handler for multiplayer button (similar structure)
   $('.multiplayer').click(function() {
-      $('#multiplayer').toggleClass('off on');
-      $('#singleplayer').addClass('off').removeClass('on');
-      $('#mainmenu').addClass('skibidi').removeClass('not-skibidi');
-      // Check if every element is off
-      if ($('#singleplayer').hasClass('off') && $('#multiplayer').hasClass('off')) {
-          $('#mainmenu').removeClass('skibidi').addClass('not-skibidi');
-      }
+    $('#multiplayer').toggleClass('off on');
+
+    $('#singleplayer').addClass('off').removeClass('on');
+    $('#settings').addClass('off').removeClass('on');
+    $('#accounts').addClass('off').removeClass('on');
+    $('#mainmenu').addClass('skibidi').removeClass('not-skibidi');
+    
+    if ($('#multiplayer').hasClass('off')) {
+      $('#mainmenu').removeClass('skibidi').addClass('not-skibidi');
+    }
   });
 
+  // Click handler for settings button (similar structure)
+  $('.settings').click(function() {
+    $('#settings').toggleClass('off on');
+    $('#singleplayer').addClass('off').removeClass('on');
+    $('#multiplayer').addClass('off').removeClass('on');
+    $('#accounts').addClass('off').removeClass('on');
+    $('#mainmenu').addClass('skibidi').removeClass('not-skibidi');
+    
+    if ($('#settings').hasClass('off')) {
+      $('#mainmenu').removeClass('skibidi').addClass('not-skibidi');
+    }
+  });
+
+  // Click handler for accounts button (similar structure)
+  $('.accounts').click(function() {
+    $('#accounts').toggleClass('off on');
+    $('#singleplayer').addClass('off').removeClass('on');
+    $('#multiplayer').addClass('off').removeClass('on');
+    $('#settings').addClass('off').removeClass('on');
+    $('#mainmenu').addClass('skibidi').removeClass('not-skibidi');
+    
+    if ($('#accounts').hasClass('off')) {
+      $('#mainmenu').removeClass('skibidi').addClass('not-skibidi');
+    }
+  });
+
+  // Click handler for realm button (unchanged)
   $('.realm').click(function() {
     $('#notification').css('animation', 'notification 5s');
   });
