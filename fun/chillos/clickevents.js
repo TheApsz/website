55eddae4a424bdb97e4f2240fe222d0cb3e894df
Apsz <oddonodeonas@gmail.com>
2024-06-05 15:16:15 +0300

    mobile fix

--- fun/chillos/clickevents.js
+++ fun/chillos/clickevents.js
@@ -1,41 +1,22 @@
 $(document).ready(function() {
-       var weatherexpand = 0;
-       var timeboxexpand = 0;
-     
-       $("#weatherboxexpand, #timeboxexpand").css("opacity", "0");
-       $("#weatherboxexpand, #timeboxexpand").css("scale", "0.9");
-       $("#weatherboxexpand, #timeboxexpand").css("translate", "0% 25%");
+  $('#menush').click(function() {
+      const menu = $('#menu');
 
-      $("#weatherbox").click(function() {
-        weatherexpand = (weatherexpand === 0) ? 1 : 0;
-          if (weatherexpand) {
-              timeboxexpand = 0;
-            $("#weatherboxexpand").css("opacity", "1");
-            $("#weatherboxexpand").css("scale", "1");
-            $("#weatherboxexpand").css("translate", "0% 0%");
-            // Hide the timeboxexpand when weatherboxexpand is clicked
-            $("#timeboxexpand").css("opacity", "0");
-            $("#timeboxexpand").css("scale", "0.9");
-            $("#timeboxexpand").css("translate", "0% 25%");
-          } else {
-            $("#weatherboxexpand").css("opacity", "0");
-            $("#weatherboxexpand").css("scale", "0.9");
-            $("#weatherboxexpand").css("translate", "0% 25%");
-      }});
-      $("#timebox").click(function() {
-       timeboxexpand = (timeboxexpand === 0) ? 1 : 0;
-         if (timeboxexpand) {
-              weatherexpand = 0;
-           $("#timeboxexpand").css("opacity", "1");
-           $("#timeboxexpand").css("scale", "1");
-           $("#timeboxexpand").css("translate", "0% 0%");
-           // Hide the timeboxexpand when weatherboxexpand is clicked
-           $("#weatherboxexpand").css("opacity", "0");
-           $("#weatherboxexpand").css("scale", "0.9");
-           $("#weatherboxexpand").css("translate", "0% 25%");
-         } else {
-           $("#timeboxexpand").css("opacity", "0");
-           $("#timeboxexpand").css("scale", "0.9");
-           $("#timeboxexpand").css("translate", "0% 25%");
-     }});
+      if (menu.css('opacity') == 0) {
+          menu.css({ opacity: 1, scale: '1' });
+      } else {
+          menu.css({ opacity: '', scale: '' });
+      }
+  });
+
+
+  $('.ind').click(function() {
+    const indicator = $(this).find('.indicator');
+    const currentWidth = parseInt(indicator.css('width'));
+
+    if (currentWidth === 0) {
+        indicator.css({ width: '25%' });
+    } else {
+        indicator.css({ width: '0%' });
+    }});
 });
