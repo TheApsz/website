eeb7e18d0c94ab63629aab9c479a43293fd51cca
TheApsz <theapsz@proton.me>
2025-04-05 15:12:47 +0300

    api changed because of conflict between stuff

--- g/aim-trainer/SRC/JS/mainmenu.js
+++ g/aim-trainer/SRC/JS/mainmenu.js
@@ -1,10 +1,2 @@
-$(document).ready(function() {
-    $('[id$="-sh"]').on('click', function() {
-        let targetId = $(this).attr('id').replace('-sh', '-select');
 
-        $('[id$="-select"]').removeClass('active');
-
-        $('#' + targetId).toggleClass('active');
-    });
-});
 
