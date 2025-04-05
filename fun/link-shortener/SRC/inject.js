cd4f24eb6b2399dd8e94fec43dcf0e83d15fb71c
TheApsz <theapsz@proton.me>
2025-04-05 20:25:47 +0300

    some new api changes again because fuck me

--- fun/link-shortener/SRC/inject.js
+++
@@ -1,4 +0,0 @@
-
-// Imports JavaScript files
-import './JS/main.js';
-
--- g/aim-trainer/SRC/JS/mainmenu.js
+++ g/aim-trainer/SRC/JS/mainmenu.js
@@ -1,2 +1,63 @@
+$(document).ready(function() {
+    $('#menuBox .button').click(function() {
+      const $this = $(this);
+  
+      if ($this.hasClass('active')) {
+        // Already active: remove after small delay
+        setTimeout(() => {
+          $this.removeClass('active');
+          $('#menuBoxBig').removeClass('active');
+          console.log('Menu & button OFF');
+        }, 10);
+      } else {
+        // Deactivate all buttons
+        $('#menuBox .button').removeClass('active');
+  
+        // Activate the clicked one
+        $this.addClass('active');
+        $('#menuBoxBig').addClass('active');
+        console.log('Menu & button ON');
+      }
+    });
+  });
+  
+  
+  
+  
+  
+  
 
+$(document).ready(function() {
+    $('#debugBorder').click(function() {
+      // Toggle active class on the button
+      $(this).toggleClass('active');
+      
+      // Toggle the CSS variable globally
+      if ($(this).hasClass('active')) {
+        $('html').css('--Debug-border', 'solid 1px purple');
+        console.log('DebugBorder ON');
+      } else {
+        $('html').css('--Debug-border', '0');
+        console.log('DebugBorder OFF');
+      }
+    });
+  });
 
+  $(document).ready(function() {
+    // When any button starting with #sh- is clicked
+    $('[id^="sh-"]').click(function() {
+      // Get the target ID (e.g., "play" from "sh-play")
+      var target = this.id.split('-')[1]; 
+      // Construct the target selector (e.g., "#playSelect")
+      var targetSelector = '#' + target + 'Select';
+      
+      // Remove 'active' from all possible targets
+      $('[id$="Select"]').removeClass('active');
+      // Add 'active' to the clicked button's target
+      $(targetSelector).addClass('active');
+      
+      // Optional: Also toggle active on the button itself
+      $('[id^="sh-"]').removeClass('active');
+      $(this).addClass('active');
+    });
+  });
